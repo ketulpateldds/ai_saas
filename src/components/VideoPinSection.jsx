6f2569fbb4b08d5c0da052d06a9fe804e7706cba
Ketul Patel <patelketul1030@gmail.com>
2026-04-16 18:08:14 +0530

    fixed

--- src/components/VideoPinSection.jsx
+++ src/components/VideoPinSection.jsx
@@ -14,7 +14,7 @@ function VideoPinSection() {
       scrollTrigger: {
         trigger: ".vd-pin-section",
         start: "0% top",
-        end: "100% top",
+        end: "150% top",
         scrub: 1.5,
         pin: true,
         anticipatePin: 1,
@@ -25,7 +25,16 @@ function VideoPinSection() {
     tl.to(".video-box", {
       clipPath: "circle(100% at 50% 50%)",
       ease: "power1.inOut",
-    });
+    }).to(
+      ".abs-center",
+      {
+        opacity: 0,
+        scale: 0.8,
+        duration: 0.5,
+        ease: "power1.inOut",
+      },
+      "<",
+    );
   }, [isMobile]);
 
   return (
@@ -40,14 +49,19 @@ function VideoPinSection() {
       >
         <video src="/videos/showcase.mp4" playsInline muted loop autoPlay />
 
-        <div className="abs-center md:scale-100 scale-200 flex-center size-20 md:size-auto">
-          <img src="/images/circle-text.svg" alt="" className="spin-circle" />
-          <div className="play-btn">
-            <img
-              src="/images/play.svg"
-              alt=""
-              className="size-[3vw] ml-[.5vw]"
-            />
+        <div className="abs-center flex-center md:scale-100 scale-200">
+          {/* Spinning Outer Ring */}
+          <img
+            src="/images/circle-text.svg"
+            alt=""
+            className="spin-circle size-[15vw]"
+          />
+
+          {/* Play Button - Now uses flex to center icon naturally */}
+          <div className="absolute inset-0 flex-center">
+            <div className="play-btn">
+              <img src="/images/play.svg" alt="" className="size-[3vw]" />
+            </div>
           </div>
         </div>
       </div>
